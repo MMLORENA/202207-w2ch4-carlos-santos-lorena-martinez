257adab9c798a9f194e24768b154d909aa52f124
Carlos <casantosmu@gmail.com>
2022-07-14 23:32:42 +0200

    Created length property on SkylabArray

--- js/SkylabArray.js
+++ js/SkylabArray.js
@@ -1,9 +1,14 @@
 class SkylabArray {
+  length;
+
   constructor(...values) {
+    let iterationCount = 0;
     // eslint-disable-next-line no-restricted-syntax
     for (const index of values.keys()) {
       this[index] = values[index];
+      iterationCount += 1;
     }
+    this.length = iterationCount;
   }
 }
 
